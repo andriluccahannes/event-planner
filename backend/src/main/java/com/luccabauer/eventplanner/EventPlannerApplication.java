0922aab4a6c8a8f37f7a7f30be13d70052b4b91d
andriluccahannes <lb214@hdm-stuttgart.de>
2023-11-12 16:51:10 +0100

    init docker, spring boot application file

--- backend/src/main/java/com/luccabauer/eventplanner/EventPlannerApplication.java
+++ backend/src/main/java/com/luccabauer/eventplanner/EventPlannerApplication.java
@@ -2,12 +2,18 @@ package com.luccabauer.eventplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+@RestController
 public class EventPlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventPlannerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String helloWorld(){return "Hello, World!";}
 }
